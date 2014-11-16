c993cba5eb838409637e047e421f3e902c14fc11
RainC <rainc.root@gmail.com>
2014-11-17 01:54:06 +0900

    Update get.php
    added tweetcount parameter, (default = 1)
    
    usage is domain.com/get.php?username=Twitter ID&tweetcount=4

--- get.php
+++ get.php
@@ -1,6 +1,4 @@
 <?php
-	//Usage : 
-	//Domain.com/get.php?username=Twitter User Name
 	function json_encode2($data) {
 		switch (gettype($data)) {
 			case 'boolean':
@@ -33,12 +31,15 @@
 	require_once("./twitteroauth/twitteroauth.php");
 	 
 	$twitteruser = $_GET["username"];
-	$notweets = 1;
-	$consumerkey = "CONSUMER_KEY";
-	$consumersecret = "CONSUMER_SECERT";
-	$accesstoken = "ACCESS_TOKEN";
-	$accesstokensecret = "ACCESS_TOKEN_SECERT";
-	
+	$notweets = $_GET["tweetcount"];
+	if ($notweets == "") {
+		$notweets = "1";
+	}
+	$consumerkey = "";
+	$consumersecret = "";
+	$accesstoken = "";
+	$accesstokensecret = "";
+	 
 	function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
 	  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
 	  return $connection;
@@ -47,13 +48,24 @@
 	$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
 	 
 	$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
+	$array = json_decode(json_encode($tweets), true);
+	// $json["text"] = $tweets[0]->text;
+	// $json["created"] = $tweets[0]->created_at;
+	// $json["favorites_count"] = $tweets[0]->favorite_count;	
+	// $json["retweet_count"] = $tweets[0]->retweet_count;
+	// $json["media_url"] = $tweets[0]->extended_entities->media[0]->indices->media_url;
+	// $json["url"] = $tweets[0]->extended_entities->media[0]->indices->display_url;
+	$count = 0;
+	foreach ($array as $posts) {
+		$json[$count]["text"] = $posts["text"];
+		$json[$count]["created"] = $posts["created_at"];
+		$json[$count]["favorite_count"] = $posts["favorite_count"];
+		$json[$count]["retweet_count"] = $posts["retweet_count"];
+		$json[$count]["url"] = $posts["extended_entities"]["media"][0]["display_url"];
+		$json[$count]["media_url"] = $posts["extended_entities"]["media"][0]["media_url"];	
+		$count = $count + 1;
+ 
+	}     
 	
-	$json["text"] = $tweets[0]->text;
-	$json["created"] = $tweets[0]->created_at;
-	$json["favorites_count"] = $tweets[0]->favorite_count;	
-	$json["retweet_count"] = $tweets[0]->retweet_count;
-
-
 	echo json_encode2($json);
-
 ?>
